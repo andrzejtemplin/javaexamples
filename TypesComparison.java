98085f54f9478cd0d4de66c811230e932f7d77af
andrzejtemplin <17392604+andrzejtemplin@users.noreply.github.com>
2021-07-21 10:58:33 +0200

    Types comparison

---
+++ TypesComparison.java
@@ -0,0 +1,11 @@
+public class TypesComparison {
+        public static void main(String... args) {
+            String greeting3 = "hello";
+            String greeting4 = "hello";
+            System.out.println(greeting3  == greeting4);
+            Long i1 = Long.valueOf("127");
+            Long i2 = Long.valueOf("127");
+            System.out.println(i1 == i2);
+
+        }
+    }
